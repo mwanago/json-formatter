c194d523d900347eeefa9030761e9129f22d3966
Marcin <wanago.marcin@gmail.com>
2024-04-21 00:30:30 +0200

    refactor: make the code style better

--- src/JsonFormatter/useJsonFormatter.tsx
+++ src/JsonFormatter/useJsonFormatter.tsx
@@ -3,26 +3,34 @@ import { ChangeEvent, useState } from 'react';
 export function useJsonFormatter() {
   const [error, setError] = useState<null | string>(null);
 
+  const formatTextAsJson = (text: string) => {
+    const data = JSON.parse(text);
+    return JSON.stringify(data, null, 2);
+  };
+
+  const downloadJson = (formattedJson: string, fileName: string) => {
+    const anchor = document.createElement('a');
+    const blob = new Blob([formattedJson], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+
+    anchor.setAttribute('href', url);
+    anchor.setAttribute('download', `${fileName}_formatted`);
+    anchor.click();
+  };
+
   const handleJsonFile = async (event: ChangeEvent<HTMLInputElement>) => {
-    const chosenFile = event.target.files?.[0];
+    const selectedFile = event.target.files?.[0];
     setError(null);
 
-    if (!chosenFile) {
+    if (!selectedFile) {
       return;
     }
 
     try {
-      const textFromFile = await chosenFile.text();
-      const data = JSON.parse(textFromFile);
-      const formattedJson = JSON.stringify(data, null, 2);
+      const textFromFile = await selectedFile.text();
+      const formattedJson = formatTextAsJson(textFromFile);
 
-      const anchor = document.createElement('a');
-      const blob = new Blob([formattedJson], { type: 'application/json' });
-      const url = URL.createObjectURL(blob);
-
-      anchor.setAttribute('href', url);
-      anchor.setAttribute('download', `${chosenFile.name}_formatted`);
-      anchor.click();
+      downloadJson(formattedJson, selectedFile.name);
     } catch {
       setError('The provided file does not contain valid JSON');
     }
